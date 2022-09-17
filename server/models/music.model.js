01cc6b514494450c5ad1d75c9bf919d8238ebd00
Lynsey Kemplin <purplez479@gmail.com>
2022-09-16 18:34:32 -0700

    Added backend setup for basic CRUD operations

---
+++ server/models/music.model.js
@@ -0,0 +1,24 @@
+// All fields added and validation to require form entry
+
+const mongoose = require('mongoose')
+
+const MusicSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Must enter song title."]
+    },
+    artist: {
+        type: String,
+        required: [true, "Must enter song title."]
+    },
+    description: {
+        type: String,
+        required: [true, "Must enter description."]
+    },
+    quote: {
+        type: String,
+        required: [true, "Must enter quote."]
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("Music", )
