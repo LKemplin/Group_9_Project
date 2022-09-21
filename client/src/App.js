e5cf954737a5901a8311ac97c89473cad9975a1f
Edwin Soto <edwin_s1106@yahoo.com>
2022-09-21 09:59:24 -0700

    styling change

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Dashboard from "./views/Dashboard";
 import AddQuotes from "./views/AddQuotes";
 import Details from "./views/Details";
 import EditQuote from "./views/EditQuote";
-import Logandreg from "./components/logandreg";
+import Logandreg from "./components/Logandreg";
 // import logo from "./logo.svg";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
